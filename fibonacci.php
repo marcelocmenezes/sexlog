0005d3b2402d53d5447992db8477a33ad70f8567
Marcelo Menezes <marcelomenezes.0907@gmail.com>
2016-01-25 11:02:18 -0200

    Create fibonacci.php

---
+++ fibonacci.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *
+ * @Author: Marcelo C Menezes
+ * https://github.com/marcelocmenezes
+ *
+ */
+
+function fib($n) {
+    $i = 1;
+    $j = 0;
+    $soma = 0;
+
+    for($k = 1; $k <= $n; $k++) {
+        $t = $i + $j;
+        $i = $j;
+        $j = $t;
+        $soma += $j;
+
+        if($k == $n) return $soma . "\n";
+    }
+}
+
+echo fib(49);
+
+?>
